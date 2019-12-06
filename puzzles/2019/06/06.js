19fff4d9a40b4c7105447270639c6834c459fa3e
Andrew Gurney <AGurney@FindMyPast.com>
2019-12-06 08:59:02 +0000

    Add orbit transfer calculator

--- puzzles/2019/06/06.js
+++ puzzles/2019/06/06.js
@@ -2,15 +2,27 @@ const {
 	calculateOrbitalChecksum,
 } = require('./orbit-checksum-counter');
 const {
+	calculateOrbitalTransfer,
+} = require('./orbit-transfer-calculator');
+const {
 	readOrbitalRelationshipsFromFile,
 } = require('./file-interactions');
 
-const solveFirstChallenge = async () => {
-	const orbitalRelationships = readOrbitalRelationshipsFromFile();;
+const solveFirstChallenge = () => {
+	const orbitalRelationships = readOrbitalRelationshipsFromFile();
 	return calculateOrbitalChecksum(orbitalRelationships);
 };
 
-const solveSecondChallenge = async () => {};
+const solveSecondChallenge = () => {
+	const orbitalRelationships = readOrbitalRelationshipsFromFile();
+	const orbitalBodyA = 'YOU';
+	const orbitalBodyB = 'SAN';
+	return calculateOrbitalTransfer(
+		orbitalBodyA,
+		orbitalBodyB,
+		orbitalRelationships
+	);
+};
 
 module.exports = {
 	'1': solveFirstChallenge,
---
+++ puzzles/2019/06/orbit-transfer-calculator.js
@@ -0,0 +1,70 @@
+const calculateOrbitalTransfer = (
+	orbitalBodyA,
+	orbitalBodyB,
+	orbitalRelationships
+) => {
+	const orbitingBodysWithPrimaryBody =
+		getOrbitingBodysWithPrimaryBody(orbitalRelationships);
+	const orbitChainToOrbitaBodyA =
+		getOrbitChain(orbitalBodyA, orbitingBodysWithPrimaryBody);
+	const orbitChainToOrbitaBodyB =
+		getOrbitChain(orbitalBodyB, orbitingBodysWithPrimaryBody);
+	const transfersRequired =
+		getTransfersRequired(
+			orbitChainToOrbitaBodyA,
+			orbitChainToOrbitaBodyB
+		);
+
+	return transfersRequired;
+};
+
+const getOrbitingBodysWithPrimaryBody = orbitalRelationships => {
+	return orbitalRelationships.reduce((accumulator, orbitalRelationship) => {
+		const [primaryBody, orbitingBody,] = orbitalRelationship;
+
+		accumulator[orbitingBody] = primaryBody;
+
+		return accumulator;
+	}, {});
+};
+
+const getOrbitChain = (orbitalBody, orbitingBodysWithPrimaryBody) => {
+	const orbitChain = [orbitalBody,];
+
+	let currentOrbitalBody = orbitalBody;
+	while (orbitingBodysWithPrimaryBody.hasOwnProperty(currentOrbitalBody)) {
+		currentOrbitalBody = orbitingBodysWithPrimaryBody[currentOrbitalBody];
+		orbitChain.unshift(currentOrbitalBody);
+	}
+
+	return orbitChain;
+};
+
+const getTransfersRequired = (
+	orbitChainA,
+	orbitChainB
+) => {
+	const shortestChainLength =
+		Math.min(orbitChainA.length, orbitChainB.length);
+	let commonChainLength = 0;
+
+	for (let i = 0; i < shortestChainLength; i++) {
+		if (orbitChainA[i] !== orbitChainB[i]) {
+			break;
+		}
+		commonChainLength = commonChainLength + 1;
+	}
+
+	// The -1 is because we want to ignore the final orbital bodies in each
+	// chain, typically you and Santa.
+
+	const transfersRequired =
+		(orbitChainA.length - 1 - commonChainLength) +
+		(orbitChainB.length - 1 - commonChainLength);
+
+	return transfersRequired;
+};
+
+module.exports = {
+	calculateOrbitalTransfer,
+};
