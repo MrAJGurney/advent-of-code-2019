f471fb3e79a5d23828fe23e22348bfa0fe786317
Andrew Gurney <AGurney@FindMyPast.com>
2019-12-06 16:52:00 +0000

    Move operation handler inside the definition

--- puzzles/2019/05/intcode-terminal.js
+++ puzzles/2019/05/intcode-terminal.js
@@ -1,46 +1,4 @@
-const OPERATIONS = {
-	add: {
-		code: 1,
-		length: 4,
-	},
-	multiply: {
-		code: 2,
-		length: 4,
-	},
-	input: {
-		code: 3,
-		length: 2,
-	},
-	output:  {
-		code: 4,
-		length: 2,
-	},
-	jumpIfTrue:  {
-		code: 5,
-		length: 3,
-	},
-	jumpIfFalse:  {
-		code: 6,
-		length: 3,
-	},
-	lessThan:  {
-		code: 7,
-		length: 4,
-	},
-	equals:  {
-		code: 8,
-		length: 4,
-	},
-	exit:  {
-		code: 99,
-		length: 1,
-	 },
-};
-
-const PARAMETER_MODES = {
-	position: 0,
-	immediate: 1,
-};
+const { OPERATIONS, } = require('./operations');
 
 const runDiagnostics = async (
 	startState,
@@ -169,104 +127,28 @@ const handleOperation = async (
 		},
 	}
 ) => {
-	if (operation.code === OPERATIONS.add.code) {
-		const [verb, noun, write,] = params;
-		const [verbMode, nounMode, writeMode,] = paramsModes;
-		throwIfParamIsInImmediateMode(writeMode, 'write');
-		memoryState[write] =
-			getParamValue(verb, verbMode, memoryState) +
-			getParamValue(noun, nounMode, memoryState);
-		return instructionPtr + operation.length;
+	const operationHandler = operation.handle;
+
+	if (typeof operationHandler !== 'function') {
+		throw new Error('Unhandled operation code');
 	}
 
-	if (operation.code === OPERATIONS.multiply.code) {
-		const [verb, noun, write,] = params;
-		const [verbMode, nounMode, writeMode,] = paramsModes;
-		throwIfParamIsInImmediateMode(writeMode, 'write');
-		memoryState[write] =
-			getParamValue(verb, verbMode, memoryState) *
-			getParamValue(noun, nounMode, memoryState);
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.input.code) {
-		const [write,] = params;
-		const [writeMode,] = paramsModes;
-		throwIfParamIsInImmediateMode(writeMode, 'write');
-		const id = await requestTerminalInput('ID');
-		memoryState[write] = id;
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.output.code) {
-		const [out,] = params;
-		const [outMode,] = paramsModes;
-		const output =
-			getParamValue(out, outMode, memoryState);
-		handleTerminalOutput(output);
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.jumpIfTrue.code) {
-		const [verb, jumpTo,] = params;
-		const [verbMode, jumpToMode,] = paramsModes;
-		if (getParamValue(verb, verbMode, memoryState) !== 0) {
-			return getParamValue(jumpTo, jumpToMode, memoryState);
-		}
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.jumpIfFalse.code) {
-		const [verb, jumpTo,] = params;
-		const [verbMode, jumpToMode,] = paramsModes;
-		if (getParamValue(verb, verbMode, memoryState) === 0) {
-			return getParamValue(jumpTo, jumpToMode, memoryState);
-		}
-		return instructionPtr + operation.length;
-	};
-
-	if (operation.code === OPERATIONS.lessThan.code) {
-		const [verb, noun, write,] = params;
-		const [verbMode, nounMode, writeMode,] = paramsModes;
-		throwIfParamIsInImmediateMode(writeMode, 'write');
-		const isLessThan =
-			getParamValue(verb, verbMode, memoryState) <
-			getParamValue(noun, nounMode, memoryState);
-		memoryState[write] = isLessThan ? 1 : 0;
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.equals.code) {
-		const [verb, noun, write,] = params;
-		const [verbMode, nounMode, writeMode,] = paramsModes;
-		throwIfParamIsInImmediateMode(writeMode, 'write');
-		const equals =
-			getParamValue(verb, verbMode, memoryState) ===
-			getParamValue(noun, nounMode, memoryState);
-
-		memoryState[write] = equals ? 1 : 0;
-		return instructionPtr + operation.length;
-	}
-
-	if (operation.code === OPERATIONS.exit.code) {
-		return instructionPtr + operation.length;
-	}
-
-	throw new Error('Unhandled operation code');
-};
-
-const getParamValue = (param, paramMode, memoryState) => {
-	return paramMode === PARAMETER_MODES.immediate ?
-		param :
-		memoryState[param];
-};
-
-const throwIfParamIsInImmediateMode = (paramMode, paramName) => {
-	if (paramMode === PARAMETER_MODES.immediate) {
-		throw new Error(
-			`Param can not be in immediate mode: ${paramName}`
-		);
-	}
+	return await operationHandler(
+		{
+			state: {
+				memoryState,
+				instructionPtr,
+			},
+			instruction: {
+				operation,
+				params,
+				paramsModes,
+			},
+			terminalIO: {
+				requestTerminalInput,
+				handleTerminalOutput,
+			},
+		});
 };
 
 module.exports = {
