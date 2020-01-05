8cc7be90e916298ec46f17b51209da4ddcbb11b0
Andrew Gurney <AGurney@FindMyPast.com>
2020-01-05 00:00:24 +0000

    Implement map scaffolds logic

--- puzzles/2019/17/scaffold-mapper.js
+++ puzzles/2019/17/scaffold-mapper.js
@@ -1,11 +1,16 @@
+const {
+	operationCodes,
+} = require('../intcode-computer/operation-codes');
+
 const buildScaffoldMapper = intcodeComputer => {
 	const self = {
 		intcodeComputer,
+		scaffolds: [],
 	};
 
 	const getAlignmentParameters = buildGetAlignmentParameters();
 
-	const mapScaffolds = buildMapScaffolds();
+	const mapScaffolds = buildMapScaffolds(self);
 
 	return Object.assign(
 		self,
@@ -16,7 +21,20 @@ const buildScaffoldMapper = intcodeComputer => {
 
 const buildGetAlignmentParameters = () => () => {};
 
-const buildMapScaffolds = () => () => {};
+const buildMapScaffolds = self => () => {
+	self.intcodeComputer.runUntil([
+		operationCodes.halt,
+	]);
+
+	const output = self.intcodeComputer.outputHeap.slice();
+
+	self.scaffolds = output
+		.map(code => String.fromCharCode(parseInt(code)))
+		.join('')
+		.trim()
+		.split('\n')
+		.map(line => line.split(''));
+};
 
 module.exports = {
 	buildScaffoldMapper,
--- puzzles/2019/17/scaffold-mapper.spec.js
+++ puzzles/2019/17/scaffold-mapper.spec.js
@@ -1,17 +1,34 @@
 const { buildScaffoldMapper, } = require('./scaffold-mapper');
 
-const mockIntcodeComputer = {};
+const mockOutput = [
+	'46', '46', '35', '46', '46', '10',
+	'35', '35', '35', '35', '35', '10',
+	'35', '46', '35', '46', '35', '10',
+	'35', '35', '35', '46', '94', '10',
+];
+
+const mockIntcodeComputer = {
+	outputHeap: mockOutput,
+	runUntil: () => {},
+};
 
 describe('scaffoldMapper', () => {
-	describe('intcode computer', () => {
-		it('exists', () => {
-			const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
-			expect(scaffoldMapper).toHaveProperty('intcodeComputer');
-		});
-		it('is the computer that is used to initialise', () => {
-			const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
-			expect(scaffoldMapper.intcodeComputer).toBe(mockIntcodeComputer);
-		});
+	describe('properties', () => {
+		const properties = [
+			['intcodeComputer',mockIntcodeComputer,],
+			['scaffolds',[],],
+		];
+
+		describe.each(properties)('%s', (name, initialValue) => {
+			it('exists', () => {
+				const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
+				expect(scaffoldMapper).toHaveProperty(name);
+			});
+			it('has expected value', () => {
+				const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
+				expect(scaffoldMapper[name]).toStrictEqual(initialValue);
+			});
+		  });
 	});
 
 	describe('getAlignmentParameters', () => {
@@ -35,6 +52,19 @@ describe('scaffoldMapper', () => {
 			const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
 			expect(typeof scaffoldMapper.mapScaffolds)
 				.toStrictEqual('function');
+		});
+		it('maps intcode output to scaffold', () => {
+			const expectedScaffolds = [
+				['.', '.', '#', '.', '.',],
+				['#', '#', '#', '#', '#',],
+				['#', '.', '#', '.', '#',],
+				['#', '#', '#', '.', '^',],
+			];
+
+			const scaffoldMapper = buildScaffoldMapper(mockIntcodeComputer);
+			scaffoldMapper.mapScaffolds();
+
+			expect(scaffoldMapper.scaffolds).toStrictEqual(expectedScaffolds);
 		});
 	});
 });
