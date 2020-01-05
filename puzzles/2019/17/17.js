0a0db81643e784bf9e095178fb5045f25db11ea3
Andrew Gurney <AGurney@FindMyPast.com>
2020-01-05 16:31:33 +0000

    Use scaffold mapper to solve puzzle

--- puzzles/2019/17/17.js
+++ puzzles/2019/17/17.js
@@ -1,4 +1,23 @@
-const solveFirstChallenge = () => {};
+const {
+	readIntcodeSoftware,
+} = require('./file-interactions');
+const {
+	buildIntcodeComputer,
+} = require('../intcode-computer/intcode-computer');
+const {
+	buildScaffoldMapper,
+} = require('./scaffold-mapper');
+
+const solveFirstChallenge = () => {
+	const softwareFile = './input.txt';
+	const software = readIntcodeSoftware(softwareFile);
+	const intcodeComputer = buildIntcodeComputer(software);
+	const scaffoldMapper = buildScaffoldMapper(intcodeComputer);
+
+	return scaffoldMapper
+		.getAlignmentParameters()
+		.reduce((accum, param) => accum + param, 0);
+};
 
 const solveSecondChallenge = () => {};
 
